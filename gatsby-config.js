f75db62073b5dd5d6feb83ac84222a83fbd6721d
Koy Carraway <koycarraway@me.com>
2020-10-27 08:03:33 -0500

    Update README and meta details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Device Mockups`,
+    description: `A constantly growing library of pixel-perfect devices for your design mockups and commercial projects.`,
+    author: `@koycarraway`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
