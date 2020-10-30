45854a5d2f22f653df8f904cd8ffa6f17a0b21d4
Koy Carraway <koycarraway@me.com>
2020-10-30 15:25:53 -0500

    Update checkout.jsx

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-// import { trackCustomEvent } from 'gatsby-plugin-google-analytics'
+import { OutboundLink } from "gatsby-plugin-google-gtag"
 
 const baseStyles = 'flex items-center justify-center px-8 py-3 border border-transparent text-base sm:text-md leading-6 font-semibold rounded-full text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out'
 
@@ -19,18 +19,14 @@ class Checkout extends React.Component {
       }],
       value: 10.00
     })
-  }    
+    console.log("Buy Now button clicked")
+  }
 
   render() {
     return (
-      <a 
-        href="https://gum.co/device-mockups?wanted=true" 
-        rel="noreferrer" 
-        className={`${baseStyles}`}
-        onClick={this.handleClick}
-      >
+      <OutboundLink href="https://gum.co/device-mockups?wanted=true" onClick={this.handleClick} className={`${baseStyles}`}>
         Buy Now · $10
-      </a>
+      </OutboundLink>
     )
   }
 }
