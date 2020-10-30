bffc2c89538cb94d2fa5d925b1f1b75c2e79a3ca
Koy Carraway <koycarraway@me.com>
2020-10-30 12:48:13 -0500

    Revert to use gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,61 +59,32 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-181585210-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        // defer: false,
-        // Any additional optional fields
-        sampleRate: 100,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "devicemockups.design",
-      },
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-RVEHB28TQP", // Google Analytics - Global Site Tag
+          "UA-181585210-1", // Google Universal Analytics
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      }
     },
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
-    //     trackingIds: [
-    //       "G-RVEHB28TQP", // Google Analytics - Global Site Tag
-    //       "UA-181585210-1", // Google Universal Analytics
-    //       // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-    //     ],
-    //     // This object gets passed directly to the gtag config command
-    //     // This config will be shared across all trackingIds
-    //     // gtagConfig: {
-    //     //   optimize_id: "OPT_CONTAINER_ID",
-    //     //   anonymize_ip: true,
-    //     //   cookie_expires: 0,
-    //     // },
-    //     // This object is used for configuration specific to this plugin
-    //     pluginConfig: {
-    //       // Puts tracking script in the head instead of the body
-    //       head: true,
-    //       // Setting this parameter is also optional
-    //       respectDNT: true,
-    //       // Avoids sending pageview hits from custom paths
-    //       exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     },
-    //   }
-    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
