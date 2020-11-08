57b063d0024f05b2e99b6f38d3c091b5d1163309
Koy Carraway <koycarraway@me.com>
2020-11-07 18:50:10 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import DeviceShowcase from "../components/device-showcase"
+// import DeviceShowcase from "../components/device-showcase"
 import SEO from "../components/seo"
 import Carousel from "../components/carousel"
 import AppleCarousel from "../components/apple-carousel"
