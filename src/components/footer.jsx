1647f8a9ecfa6cbcc2f6965888b9fea83d8d8f51
Koy Carraway <koycarraway@me.com>
2021-04-21 19:41:15 -0500

    Moved footer out of layout and into own component

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Koymoji from "../images/koymoji@2x.png"
+import { OutboundLink } from "gatsby-plugin-google-gtag"
+
+const Footer = () => (
+  <footer className={`m-auto text-center pb-10`} >
+    <OutboundLink href="https://twitter.com/koycarraway" target="_blank" rel="noreferrer">
+      <img src={Koymoji} alt="Koy Carraway" className={`rounded-full m-auto mb-4`} width="48" height="48" />
+    </OutboundLink>
+    <p className={`m-0 text-sm font-medium text-gray-500`} >
+      Designed and built by <OutboundLink href="https://twitter.com/koycarraway" target="_blank" rel="noreferrer" className="text-black transition duration-150 ease-in-out">Koy Carraway</OutboundLink>
+    </p>
+    <small className={`m-0 text-xs text-gray-600`}>Copyright © {new Date().getFullYear()} Koy Carraway. All rights reserved.</small>
+  </footer>
+)
+
+export default Footer
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -8,12 +8,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Koymoji from "../images/koymoji@2x.png"
 
 import Header from "./header"
 import "./layout.css"
 import "./carousel.css"
-import { OutboundLink } from "gatsby-plugin-google-gtag"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -31,15 +29,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div className={`m-auto max-w-lg sm:max-w-xl md:max-w-3xl lg:max-w-full`} >
         <main>{children}</main>
-        <footer className={`m-auto text-center pb-10`} >
-          <OutboundLink href="https://twitter.com/koycarraway" target="_blank" rel="noreferrer">
-            <img src={Koymoji} alt="Koy Carraway" className={`rounded-full m-auto mb-4`} width="48" height="48" />
-          </OutboundLink>
-          <p className={`m-0 text-sm font-medium text-gray-500`} >
-            Designed and built by <OutboundLink href="https://twitter.com/koycarraway" target="_blank" rel="noreferrer" className="text-gray-100 transition duration-150 ease-in-out hover:text-indigo-400">Koy Carraway</OutboundLink>
-          </p>
-          <small className={`m-0 text-xs text-gray-600`}>Copyright © {new Date().getFullYear()} Koy Carraway. All rights reserved.</small>
-        </footer>
+        <Footer />
       </div>
     </>
   )
