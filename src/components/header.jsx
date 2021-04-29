c94422b2263c661bcb6ca5c4cfe55ff8f912d256
Koy Carraway <koycarraway@me.com>
2021-04-28 22:24:36 -0500

    Improved header dark-mode and image support

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ const Header = ({ className }) => {
   const data = useStaticQuery(
     graphql`
       query {
-        desktop: file(relativePath: { eq: "hero-bg-dark@2x.png" }) {
+        desktop: file(relativePath: { eq: "hero-bg@2x.png" }) {
           childImageSharp {
             fluid(quality: 90, maxWidth: 1920) {
               ...GatsbyImageSharpFluid_withWebp
@@ -34,26 +34,27 @@ const Header = ({ className }) => {
       className={className}
       fluid={imageData}
       fadeIn='soft'
-      backgroundColor={`#000`}
     >
       <div className={`header-content m-auto text-center max-w-full pt-12 sm:pt-20 pb-4 md:pb-0 px-4`} >
-        <h1 className={`font-semibold text-xl sm:text-xxl md:text-xxxl tracking-tight text-white`} >
-          Device Mockups
-        </h1>
-        <p className={`mb-6 text-base sm:text-md md:text-lg text-gray-400 dark:text-gray-400 m-auto max-w-lg md:max-w-xl`}>
-        A growing library of pixel-perfect mobile, tablet, and desktop devices for your design mockups and commercial projects.
-        </p>
-        <div className={`max-w-md mx-auto flex justify-center`}>
-          <Checkout />
-        </div>
-        <div className={`hidden sm:block mt-16 opacity-50 text-gray-900 dark:text-white`}>
-          <GoogleLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
-          <SonyLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
-          <MicrosoftLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
-          <AppleLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
-          <SamsungLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
-          <AmazonLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
-          <OnePlusLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
+        <div className={`relative z-20`}>
+          <h1 className={`font-semibold text-xl sm:text-xxl md:text-xxxl tracking-tight text-dark dark:text-white`} >
+            Device Mockups
+          </h1>
+          <p className={`mb-6 text-base sm:text-md md:text-lg text-gray-700 dark:text-gray-300 m-auto max-w-lg md:max-w-xl`}>
+          A growing library of pixel-perfect mobile, tablet, and desktop devices for your design mockups and commercial projects.
+          </p>
+          <div className={`max-w-md mx-auto flex justify-center`}>
+            <Checkout />
+          </div>
+          <div className={`z-10 hidden sm:block mt-16 opacity-50 text-gray-900 dark:text-white`}>
+            <GoogleLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
+            <SonyLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
+            <MicrosoftLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
+            <AppleLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
+            <SamsungLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
+            <AmazonLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
+            <OnePlusLogo className={`inline-block sm:mx-3 md:mx-4 my-2 sm:h-7`} />
+          </div>
         </div>
       </div>
     </BackgroundImage>
